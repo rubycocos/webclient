621358488288d165649acddff690a1f53b18ffaa
Gerald Bauer <gerald.bauer@gmail.com>
2014-02-28 23:44:44 +0100

    up test get

--- test/test_get.rb
+++ test/test_get.rb
@@ -5,18 +5,37 @@ require 'helper'
 class TestGet < MiniTest::Unit::TestCase
 
   def test_get
-
+    url = 'https://raw.github.com/rubylibs/fetcher/master/README.md'
     worker = Fetcher::Worker.new
-    res = worker.get( 'https://raw.github.com/rubylibs/fetcher/master/README.md' )
-
+    res = worker.get( url )
     pp res
 
-    assert_equal '200', res.code             # => '200' 
-                               #  Note: Returned (status) code is a string e.g. '404'
-    assert_equal 'OK', res.message          # => 'OK'
-    # puts response.body
-    # puts response.content_type     # =
-
+    assert_equal '200', res.code             # note: returned code is a string e.g. '200' not 200
+    assert_equal 'OK',         res.message
+    assert_equal 'text/plain', res.content_type
+    assert_equal File.read( './README.md' ), res.body   # local README.md should match remote README.md
   end
+
+  def test_get_with_socket_error
+    url = 'http://pragdave.blogs.pragprog.com/pragdave/atom.xml'
+    worker = Fetcher::Worker.new
+
+    # note: will raise SocketError -- getaddrinfo: Name or service not known
+    assert_raises( SocketError ) do
+      res = worker.get( url )
+      pp res
+    end
+  end
+
+  def test_get_not_found
+    url = 'http://blog.engineyard.com/category/ruby/feed'
+    worker = Fetcher::Worker.new
+    res = worker.get( url )
+    pp res
+
+    # note: feed link/url no longer exists, thus, 404 -- not found
+    assert_equal '404', res.code
+  end
+
 
 end # class TestCopy
